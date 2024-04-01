390662686b5e5abc57736c520ce46e67ec49b2b4
Amy Abela <amylabela@gmail.com>
2024-04-01 11:55:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector("button").addEventListener("click", function(){
+     // Set the URL you want to navigate to
+     var newUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; // Replace this with the URL you want to navigate to
+
+     // Navigate to the new URL
+     window.location.href = newUrl;
+});
